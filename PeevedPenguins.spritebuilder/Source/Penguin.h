2078bd11ea73d84c8a6261980f31cd3b3cb9933b
Jigar <github@jigarsavla.com>
2014-11-06 09:22:41 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Jigar Savla on 11/5/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,25 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Jigar Savla on 11/5/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+-(id) init
+{
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin Created");
+    }
+    
+    return self;
+}
+
+
+@end
