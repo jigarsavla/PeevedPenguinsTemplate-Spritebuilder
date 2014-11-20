b914e1e582e1b39639f9105c489df884ed9fe42e
Jigar <github@jigarsavla.com>
2014-11-20 15:51:16 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -16,7 +16,7 @@
     CCNode *_contentNode;
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
-    CCPhysicsJoint *_mosueJoint;
+    CCPhysicsJoint *_mouseJoint;
 }
 
 // is called when CCB file has completed loading
@@ -32,11 +32,51 @@
     
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
+    _mouseJointNode.physicsBody.collisionMask = @[];
 }
 
 // called on every touch in this scene
-- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
-    [self launchPenguin];
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+        //[self launchPenguin];
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    //start catapult dragging when a touch inside of the catapult arm occurs
+    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation)) {
+        
+        // move the mouseJointNode to the touch location
+        _mouseJointNode.position = touchLocation;
+        
+        //setup a spring joint between the mouseJointNode and catapult arm
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0,0) anchorB:ccp(34,138) restLength:0.f stiffness:3000.f damping:150.f];
+    }
+}
+
+- (void) touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    // whenever a touch moves, update the mouseJointNode location
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    //start catapult dragging when a touch inside of the catapult arm occurs
+    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation)) {
+        
+        // move the mouseJointNode to the touch location
+        _mouseJointNode.position = touchLocation;
+    }
+}
+
+- (void) releaseCatapult
+{
+    if (_mouseJoint != nil) {
+        // releases the joint and lets the catapult snap back
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+    }
+}
+
+- (void) touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    [self releaseCatapult];
 }
 
 - (void)launchPenguin {
